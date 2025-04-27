f61f34861cae3e2502779960c23c00749057be2d
hattie2801 <hattie2801@yahoo.com>
2025-04-27 18:16:34 -0500

    Updated part4-arrays.js

--- part4-arrays.js
+++ part4-arrays.js
@@ -1,8 +1,23 @@
 // Problem 1: Favorite Foods
 
-let food = ["Pizza", "Banana Pudding", "Sushi", "Ice Cream", "Pasta" ]
+let food = ["Pizza", "Banana Pudding", "Sushi", "Ice Cream", "Pasta"];
 food.push("Po-Boys");
 
 console.log(food[0]);
 console.log(food[food.length - 1]);
 console.log(food);
+
+
+// Problem 2: Guest List
+
+let guest = ["Alice", "Bill", "Carmen", "Daniel", "Mark", "Shane", "Margo"];
+guest.shift();
+guest.push("Larry");
+
+console.log(guest);
+console.log(guest.length);
+
+
+//Problem 3: Weekend Tasks
+
+let tasks = ["Laundry", "Groceries", "Coding", "Naps"]
