fbc04e905017c1d11bb175f94ba9667e33ea24c9
hattie2801 <hattie2801@yahoo.com>
2025-04-27 17:05:31 -0500

    Updated part4-arrays.js

---
+++ part4-arrays.js
@@ -0,0 +1,8 @@
+// Problem 1: Favorite Foods
+
+let food = ["Pizza", "Banana Pudding", "Sushi", "Ice Cream", "Pasta" ]
+food.push("Po-Boys");
+
+console.log(food[0]);
+console.log(food[food.length - 1]);
+console.log(food);
