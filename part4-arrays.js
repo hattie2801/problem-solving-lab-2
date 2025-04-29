04e0a710377f5bda99f451d9229e3290b7460f4d
hattie2801 <hattie2801@yahoo.com>
2025-04-29 02:23:04 -0500

    Updated part4-arrays.js

--- part4-arrays.js
+++ part4-arrays.js
@@ -20,4 +20,42 @@ console.log(guest.length);
 
 //Problem 3: Weekend Tasks
 
-let tasks = ["Laundry", "Groceries", "Coding", "Naps"]
+let tasks = ["Laundry", "Groceries", "Coding", "Naps"];
+
+for (let task of tasks) {
+   console.log(tasks)
+}
+
+for(let i = 0; i < tasks.length; i++){
+    console.log(tasks[i]);
+}
+
+    for (let i = 0; i < tasks.length; i++) {
+      console.log(`${i + 1}. ${tasks[i]}`);
+    }
+  
+
+    //Problem 4: Reverse It (Manual Reverse)
+
+    let numbers = ["1", "2", "3", "4", "5"];
+
+    console.log(numbers);
+
+    for(let i = numbers.length - 1; i>= 0; i--){
+        console.log(numbers[i]);
+    }
+
+
+    //Bonus: Random Picker - I am missing making random a function. I am lost on this one! 
+     
+    //let lunches =["Chicken", "Burger", "Sushi", "Salad"];
+
+    //let random = Math.floor(Math.random() * lunches.length);
+
+    //console.log(random(lunches))
+
+  
+    
+
+
+
